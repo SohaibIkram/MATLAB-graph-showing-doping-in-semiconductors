e0794eaaec87bb92c43817e575ff5b62c52daf57
Sohaib Ikram <sohaib.ikram7@gmail.com>
2020-10-24 18:20:24 +0500

    Update Command_code_2.m

--- Command_code_2.m
+++ Command_code_2.m
@@ -4,14 +4,14 @@
 Ni_Si = 1.5e10;
 
 % Donar atoms Nd for Si are being ranged from 5.5e9 to 2.5e10 to see the variation of Nn and Pn
-Nd_Si = 10^13: 100000000: 10^16;
+Nd_Si = 10^10: 100000000: 10^12;
 
 % Parameters of mobility for Si are
 meu_n_Si = 1350;
 meu_p_Si = 480;
 
 % Calling the function 'resistivity' to get resisitivity for this range of Nd for Si
-[res] = resistivity(Ni_Si, Nd_Si, meu_n_Si, meu_p_Si);
+[res, res_int] = resistivity(Ni_Si, Nd_Si, meu_n_Si, meu_p_Si);
 
 %Calling the function 'graph' to get the plot
-graph(Nd_Si,res);
+graph_2(Nd_Si,res, res_int);
