b06dd7e14d5f9090f9b74c14a92217f970464783
Sohaib Ikram <sohaib.ikram7@gmail.com>
2020-10-24 18:21:19 +0500

    Update graph_2.m

--- graph_2.m
+++ graph_2.m
@@ -1,10 +1,10 @@
 % Graph to show the vairation of Nn and Pn with Nd
-function graph_2(X,Y)
-plot(X,Y)  % plots the graph
+function graph_2(X,Y,Z)
+plot(X,Y,X,Z,'r')  % plots the graph
 
 % Setting x and y limits to get a better idea
-xlim([0 2e14])
-ylim([0 450])
+xlim([1e10 2e11])
+ylim([2e4 2.5e5])
 
 % Adding labels and titles
 title('Variation of Resistance with increase of Donor atoms')
@@ -12,6 +12,6 @@ xlabel('Donor atoms concentration')
 ylabel('Resistance of Si')
 
 % Adding legend
-legend({'Y = Resistance (decreasing)'}, 'Location','northeast')
+legend({'Y = Resistance (decreasing)', 'Y = Resistivity of intrinsic Si'}, 'Location','northeast')
 
 
