c4c83d89ce65b2debe7204502415e22f2c64965d
Sohaib Ikram <sohaib.ikram7@gmail.com>
2020-11-02 19:17:15 +0500

    Update graph.m

--- graph.m
+++ graph.m
@@ -2,17 +2,15 @@
 function graph(X,Y,Z,Ni)
 Ni_matrix(1:1951) = Ni;
 plot(X,Y,X,Z,'r',X,Ni_matrix,'g')  % plots the graph
-
+ 
 % Setting x and y limits to get a better idea
-xlim([5.5e9 2.5e10])
+xlim([1.5e10 2.5e10])
 ylim([0.5e10 2.5e10])
-
+ 
 % Adding labels and titles
 title('Variation of Nn and Np with concentration of Donor atoms')
-xlabel('Donor atoms concentration')
-ylabel('Concentration of electrons and holes')
-
+xlabel('Donor atoms concentration (Nd) per cm^3')
+ylabel('Concentration of electrons (Nn) and holes (Np) per cm^3')
+ 
 % Adding legend
-legend({'Y = Electron concentration', 'Y = Holes concentration', 'Ni = Concentration in intrinsic Si'}, 'Location','southeast')
-
-
+legend({'Nn = Electron concentration', 'Np = Holes concentration', 'Ni = Concentration of charge carriers in intrinsic Si'}, 'Location','southeast')
